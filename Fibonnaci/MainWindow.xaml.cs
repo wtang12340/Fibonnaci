686d489a9947c549d67f603544459337db20a133
wtang12340 <wtang12340@gmail.com>
2014-10-26 16:26:40 -0400

    added files

---
+++ Fibonnaci/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Fibonnaci
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+
+            
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int n = Convert.ToInt32(NBox.Text);
+                string s = "";
+                for (int i = 0; i < n; i++)
+                {
+                    s += Fibonacci(i).ToString() + " ";
+                }
+                OutBox.Text = s;
+            }
+            catch (FormatException ex)
+            {
+                OutBox.Text = "Invalid Input";
+            }
+
+
+        }
+
+
+        public int Fibonacci(int n)
+        {
+            int a = 0;
+            int b = 1;
+            for (int i = 0; i < n; i++)
+            {
+                int temp = a;
+                a = b;
+                b = temp + b;
+            }
+            return a;
+        }
+
+
+    }
+}
